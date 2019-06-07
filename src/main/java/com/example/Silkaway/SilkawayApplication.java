a36ec482a4e2210091f6a8d6142ce244294c9c97
elka <elmira.kurmanbekova@iaau.edu.kg>
2019-06-07 19:54:06 +0600

    Initial commit

---
+++ src/main/java/com/example/Silkaway/SilkawayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Silkaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilkawayApplication.class, args);
+	}
+
+}
